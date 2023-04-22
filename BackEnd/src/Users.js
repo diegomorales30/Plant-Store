c4e409b52dabfb919c48f5ea94274af0fbfbcc61
rs404655 <111717691+rs404655@users.noreply.github.com>
2023-04-22 17:16:52 -0600

    Router for User

---
+++ BackEnd/src/Users.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const User = require('User')
+const router = express.Router()
+
+router.post('/', async (req, res) =>{
+
+    let user = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+    try{
+        user = await user.save()
+        res.redirect(`/users/${user}`)
+    } catch(e){
+        //res.render('users/new', {user: user})
+    }
+
+})
