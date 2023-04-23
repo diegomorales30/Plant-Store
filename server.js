936834de0de13fd1191172454a8b64d02990dcb5
rs404655 <111717691+rs404655@users.noreply.github.com>
2023-04-23 16:19:54 -0600

    Changed routing in server js and added additional functions

--- server.js
+++ server.js
@@ -1,21 +1,34 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const User = require('/BackEnd/src/User')
+
 const usersRouter = require('/BackEnd/src/Users')
 const methodOverride = require('method-override')
 const app = express()
+const port = 3000
 
-mongoose.connect('mongodb://localhost/Users', {
-    useNewUrlParser: true, useUnifiedTopology: true
-})
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017')
+}
+
+main().then(function() {
+    console.log("Mongoose connected!");
+}).catch(err => console.log(err));
+
+var User = require("./BackEnd/src/User");
 
 app.set('view engine', 'ejs')
-app.use(express.urlencoded({extended: false}))
-app.use(methodOverride('_method'))
+app.use(express.urlencoded())
+app.use(express.json())
+app.set('views', path.join(__dirname, 'FrontEnd/views'));
+app.use(express.static(path.join(__dirname, 'FrontEnd')));
 
 app.get('/', async (req, res) => {
     const users = await User.find().sort({createdAt: 'desc'})
-    res.render('FrontEnd/views/index', {books: books})
+    res.render('FrontEnd/views/index', {User: Users})
+})
+
+app.listen(port, function(){
+    console.log("App listening on port " + port + "!")
 })
 
 app.use('users/index', usersRouter)
