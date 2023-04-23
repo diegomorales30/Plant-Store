090b6e401d759408fef0685a44f1c98d005f2404
rs404655 <111717691+rs404655@users.noreply.github.com>
2023-04-23 16:23:56 -0600

    Changed routing for userRouter

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 
-const usersRouter = require('/BackEnd/src/Users')
+const usersRouter = require('./BackEnd/src/Users')
 const methodOverride = require('method-override')
 const app = express()
 const port = 3000
