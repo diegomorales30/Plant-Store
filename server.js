dfe55c51e595178f32e83eee9a6bde9623ccd9f9
Robert Stolworthy <RAStolworthy@outlook.com>
2023-04-24 02:31:02 -0600

    Changing line 26 users/users

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'FrontEnd')));
 
 app.get('/', async (req, res) => {
     const users = await User.find().sort({createdAt: 'desc'})
-    res.render('/index', {users: Users})
+    res.render('/index', {users: users})
 })
 
 app.listen(port, function(){
