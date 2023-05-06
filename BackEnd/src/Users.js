b89aa6be49173da2198acfc46a649c3a3e2f65e3
rs404655 <111717691+rs404655@users.noreply.github.com>
2023-05-06 14:46:41 -0600

    Fixed The Login Added functionality to the index.ejs and created a get function to fetch the user with matching email and password Also changed About to an Ejs to make it dynamic to show user is logged in through the logo.

--- BackEnd/src/Users.js
+++ BackEnd/src/Users.js
@@ -3,12 +3,14 @@ const User = require('./User')
 const router = express.Router()
 
 router.post('/', async (req, res) =>{
-
-    let user = new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
-    })
+    if(User.find({ email: req.body.email}) == null){
+        let user = new User({
+         username: req.body.username,
+         email: req.body.email,
+         password: req.body.password
+        })
+    }
+    else res.redirect('/')
     try{
         user = await user.save()
         res.redirect(`/About`)
@@ -17,5 +19,12 @@ router.post('/', async (req, res) =>{
     }
 
 })
+
+router.get('/:email', async (req, res) =>{
+    const user = await User.findOne({email : req.query.email, password : req.query.password})
+    if(user == null) res.redirect('/')
+    res.render('/About', {user:user})
+})
+
 
 module.exports = router;
