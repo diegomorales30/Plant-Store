485bd820a0ef9120a6b2a9566d483480638d50fd
diegomorales30 <diegovmo96@gmail.com>
2023-05-11 10:41:50 -0600

    add the comment for the addtocart method

--- BackEnd/src/PlantServer.js
+++ BackEnd/src/PlantServer.js
@@ -36,13 +36,22 @@ app.get("/plant", async (req, res) => {
 });
 
 //Ex http://localhost:3000/user?=name&&plantpid=picturename&&useramount=3
+// adds a plant to the user cart and the only way to add plants to a shopping cart
+// if that you have to be logged in
 app.get("/addtocart", async function(req,res) {
+    // finds the plant from the data base 
     var plant = await plantModel.findOne({Picture:req.query.plantpid}).lean();
+    //finds the user from the data base
     var user = await User.findOne({username:req.query.user}).lean();
+    // Gets the limit from the quanity field in the data base
     var plantlimit = plant.Quantity;
+
+    // checks to see if user amount is greater than the plantlimit
+    // if it is then the the user has to get a different amount to add to thier cart
     if (req.query.useramount > plantlimit) {
         res.send("User demand is greater than quanity aviable");
     }
+    // if the useramount is less than plantlimit then the item is added to thier shopping cart
     else {
         plant.Quantity = req.query.useramount;
         user.cart.push(plant);
